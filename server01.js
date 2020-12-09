f7013262181aa67cbd881fc7f06a001a918eb8d1
Kemot-S <ts.tygrys@gmail.com>
2020-12-10 00:10:00 +0100

    commit portu

--- server01.js
+++ server01.js
@@ -3,6 +3,7 @@
 const { request } = require("express");
 var express = require("express")
 var app = express()
+var PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 const PORT = 3000;
 var formidable = require('formidable');
 var bodyParser = require("body-parser")
